fcf0eb55c542fc6af46b80a4b926ea87e1d93a2c
stacey-westra <stacey.westra@gmail.com>
2025-02-11 10:38:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let hasRefreshed = sessionStorage.getItem('key');
+
+function rollDice() {
+    if (hasRefreshed === '1'){
+        const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+        document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+    
+   
+        if (randomNumber1 < randomNumber2) {
+            document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+        } else if (randomNumber1 > randomNumber2) {
+            document.querySelector("h1").innerHTML = "🚩 Player 1 wins!"
+        } else 
+            document.querySelector("h1").innerHTML = "Draw!"
+        } else {    
+            sessionStorage.setItem('key', '1');
+    }
+}
+
+window.onload = rollDice();
